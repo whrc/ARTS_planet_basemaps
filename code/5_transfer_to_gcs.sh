5e55ada7598f77a4455b99de12e91e7bf454b57d
HRodenhizer <heidi.rodenhizer@yahoo.com>
2025-12-03 16:10:19 -0800

    removes old scripts to download orders and transfer files to GCS that are no longer needed

--- code/5_transfer_to_gcs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcloud storage cp --recursive /home/hrodenhizer/Documents/permafrost_pathways/rts_mapping/ARTS_planet_basemaps/data/download_20250715/global_quarterly_2017q3_mosaic/ gs://abrupt_thaw/planet_basemaps/global_quarterly/
